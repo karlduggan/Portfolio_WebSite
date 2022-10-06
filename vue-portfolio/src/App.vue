8c9fd6fd2370db057a7a66ed978df453da19441d
Karl Duggan <52557899+karlduggan@users.noreply.github.com>
2022-10-06 18:18:41 +0100

    particles update

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <vue-particles
+        color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="200"
+        shapeType="circle"
+        :particleSize="2"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="false"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed=".30"
+        :hoverEffect="false"
+        hoverMode="grab"
+        :clickEffect="false"
+        clickMode="push"
+      >
+      </vue-particles>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+   
+}
 }
 </script>
 
--- vue-portfolio/src/main.js
+++ vue-portfolio/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import VueParticles from 'vue-particles'
+
+
+createApp(App)
+.use(VueParticles)
+.mount('#app')
