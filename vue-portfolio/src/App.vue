6239335e70365d67c7523e187d5f146be4a3d207
“Karl <karlduggan_55@hotmail.com>
2023-03-16 17:35:08 +0000

    updated

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -25,26 +25,41 @@
     <div class="relative">
       <nav class="text-white py-10  flex justify-between mb-20 ">
         <h1 class="text-white text-xl text-center">Placeholder</h1>
-        <ui class="flex item-center">
+        <ui class="flex item-center list-none">
           <li>1</li>
+          <li>2</li>
           <li>
             <a href="#" class="bg-gradient-to-r from-cyan-400 to-teal-400 px-4 py-2 rounded-md ml-8 font-bold cursor-pointer z-10">Contact</a>
           </li>
         </ui>
       </nav>
       <div class="text-center ">
-        <h1 class="text-white py-2 font-bold text-5xl ">Karl Duggan</h1>
+        <h1 class="text-cyan-400 py-2 font-bold text-5xl ">Karl Duggan</h1>
         <h2 class="text-white py-2 text-2xl ">Software Developer</h2>
         <p class="text-white text-md py-5 leading-8">
-          Freelance software and web developer, creating software solution for your buisness needs.</p>
+          Freelance software and web developer, creating software solution for your buisness needs.
+        </p>
+        <ui class="flex justify-center list-none py-2 gap-6">
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" class="fill-cyan-400" width="40" height="40" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" class="fill-cyan-400" width="40" height="40" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-cyan-400" width="45" height="45" viewBox="0 0 24 24"  fill="none" stroke="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+          </li>
+        </ui>
       </div>
     </div>
     </section>
-    <section class="relative  min-h-screen w-[100%]">
-  
+
+    <section class="relative  min-h-screen w-[100%] bg-gradient-to-b from-[rgba(44,21,69,1)]  to-[rgba(9,4,16,1)]  text-white py-10">
+      <h1 class="py-2 text-3xl">About</h1>
     </section>
-    <section class="relative  min-h-screen w-[100%]">
-  
+
+    <section class="relative  min-h-screen w-[100%] bg-[rgba(9,4,16,1)] text-white py-10">
+      <h1 class="py-2 text-3xl">Projects</h1>
     </section>
     
 
