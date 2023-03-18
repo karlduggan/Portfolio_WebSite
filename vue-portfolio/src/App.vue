7d71d4f6e287d146e6e3027a7076e4e1d1f6b507
Karl Duggan <karlduggan_55@hotmail.com>
2023-03-18 00:40:58 +0000

    updated

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -33,7 +33,7 @@
           </li>
         </ui>
       </nav>
-      <div class="text-center relative top-50">
+      <div class="text-center pt-20">
         <h1 class="text-cyan-400 py-2 font-bold text-5xl ">Karl Duggan</h1>
         <h2 class="text-white py-2 text-2xl ">Software Developer</h2>
         <p class="text-white text-md py-5 leading-8">
@@ -41,36 +41,62 @@
         </p>
         <ui class="flex justify-center list-none py-2 gap-6">
           <li>
-            <svg xmlns="http://www.w3.org/2000/svg" class="fill-cyan-400" width="40" height="40" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+            <a href="https://www.github.com/karlduggan">
+              <svg xmlns="http://www.w3.org/2000/svg" class="fill-cyan-400" width="40" height="40" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+            </a>
           </li>
           <li>
-            <svg xmlns="http://www.w3.org/2000/svg" class="fill-cyan-400" width="40" height="40" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+            <a href="https://www.linkedin.com/in/karl-duggan-3666261ba/">
+              <svg xmlns="http://www.w3.org/2000/svg" class="fill-cyan-400" width="40" height="40" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+            </a>
           </li>
           <li>
-            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-cyan-400" width="45" height="45" viewBox="0 0 24 24"  fill="none" stroke="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-cyan-400" width="60" height="45" viewBox="2 3 20 20"  fill="none" stroke="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
           </li>
         </ui>
       </div>
     </div>
     </section>
 
-    <section class="snap-start px-10 min-h-screen w-[100%] bg-gradient-to-b from-[rgba(44,21,69,1)]  to-[rgba(9,4,16,1)]  text-white py-10">
-      <h1 class="text-cyan-400 py-4 font-bold text-4xl ">About</h1>
-      <p class="leading-8">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <section class="snap-start px-10 min-h-screen w-[100%] bg-[rgba(44,21,69,1)]  text-white py-10">
+      <div class="grid grid-cols-2 gap-x-40 grid-flow-row">
+        <div>
+          <h1 class="text-cyan-400 py-4 font-bold text-4xl text-left underline underline-offset-8">About</h1>
+          <p class="text-left leading-8">
+            I am a software developer currently based in Brighton UK, passionate about exploring and transforming creative ideas into reality with technologies such as Python, SQL, Javascript, HTML and CSS.<br>
+            Experienced in designing and developing desktop applications to help boost productivity to daily buisness operations.<br>
+            I also have experience writing Python scripts and SQL queries for manipulation and extraction on large datasets for data visual presentation and data analysing.<br>
+          </p>
+        </div>
+        <div>
+          
+        </div>
+        <div>
+          
+        </div>
+        <div>
+          <h1 class="text-cyan-400 py-4 font-bold text-4xl text-left underline underline-offset-8">Skills</h1>
+        </div>
+      </div>
     </section>
 
-    <section class="snap-start px-10 relative min-h-screen w-[100%] bg-white text-slate py-10">
+    <section class="snap-start px-10 relative min-h-screen w-[100%] bg-gradient-to-b from-[rgba(44,21,69,1)] to-[rgba(174,127,217,1)] text-slate py-10">
       
-      <h1 class="text-cyan-400 py-4 font-bold text-4xl ">Projects</h1>
+      <h1 class="text-cyan-400 py-4 font-bold text-4xl text-left underline underline-offset-8">Projects</h1>
     
       
     </section>
     
-    <section class="snap-start px-10 relative min-h-screen w-[100%] bg-gradient-to-b from-[rgba(44,21,69,1)]  to-[rgba(9,4,16,1)] text-white py-10">
-      
-      <h1 class="text-cyan-400 py-4 font-bold text-4xl ">Contact</h1>
-  
-    
+    <section class="snap-start relative min-h-screen w-[100%] bg-white text-slate py-10">
+      <div class="px-10 ">
+        <h1 class="text-cyan-400 py-4 font-bold text-5xl ">Send me a message!</h1>
+        <h2 class="text-slate py-2 text-2xl ">Got a question or proposal, or just want <br> to say hello? Go ahead.</h2>
+        <ContactSection></ContactSection>
+      </div>
+
+      <footer class="bg-[rgba(44,21,69,1)] bottom-0 absolute w-[100%] text-white h-40">
+        <p>d</p>
+      </footer>
   </section>
 
   </div>
@@ -78,13 +104,13 @@
 
 <script>
 
-
+import ContactSection from './components/ContactSection.vue';
 
 
 export default {
   name: 'App',
   components: {
-  
+    ContactSection
    
 }
 }
--- vue-portfolio/src/components/ContactSection.vue
+++ vue-portfolio/src/components/ContactSection.vue
@@ -1,81 +1,69 @@
 <template>
- 
-    <div class="background px-10 ">
-      <nav class="text-white py-10 mb-24 flex justify-between  ">
-        <h1 class="text-white text-xl text-center">Placeholder</h1>
-        <ui class="flex item-center">
-          <li>1</li>
-          <li>
-            <a href="#" class="bg-gradient-to-r from-cyan-400 to-teal-400 px-4 py-2 rounded-md ml-8 font-bold cursor-pointer">Contact</a>
-          </li>
-        </ui>
-      </nav>
-      <div class="text-center ">
-        <h1 class="text-white py-2 font-bold text-5xl ">Karl Duggan</h1>
-        <h2 class="text-white py-2 text-2xl ">Software Developer</h2>
-        <p class="text-white text-md py-5 leading-8">
-          Freelance software and web developer, creating software solution for your buisness needs.</p>
-      </div>
-      
-  
+    <div
+  class="block max-w-md rounded-lg bg-white p-6 shadow-lg ">
+    <form>
+        <div class="relative mb-6" data-te-input-wrapper-init>
+        <input
+            type="text"
+            class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            id="exampleInput7"
+            placeholder="Name" />
+        <label
+            for="exampleInput7"
+            class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+            >Name
+        </label>
+        </div>
+        <div class="relative mb-6" data-te-input-wrapper-init>
+        <input
+            type="email"
+            class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            id="exampleInput8"
+            placeholder="Email address" />
+        <label
+            for="exampleInput8"
+            class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+            >Email address
+        </label>
+        </div>
+        <div class="relative mb-6" data-te-input-wrapper-init>
+        <textarea
+            class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            id="exampleFormControlTextarea13"
+            rows="3"
+            placeholder="Message"></textarea>
+        <label
+            for="exampleFormControlTextarea13"
+            class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+            >Message
+        </label>
+        </div>
+        <div
+        class="mb-6 flex min-h-[1.5rem] items-center justify-center pl-[1.5rem]">
+        <input
+            class="relative float-left mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300  outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary  checked:bg-primary  checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent"
+            type="checkbox"
+            value=""
+            id="exampleCheck10" />
+        <label
+            class="inline-block pl-[0.15rem] hover:cursor-pointer"
+            for="exampleCheck10">
+            Send me a copy of this message
+        </label>
+        </div>
+        <button
+        type="submit"
+        class="w-full rounded bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+        data-te-ripple-init
+        data-te-ripple-color="light">
+        Send
+        </button>
+    </form>
     </div>
-        <!-- Particle Animation -->
-  
-      <vue-particles
-          class=" absolute w-[100%] z-0 min-h-screen"
-          color="#fff"
-          :particleOpacity="0.7"
-          :particlesNumber="200"
-          shapeType="circle"
-          :particleSize="2"
-          linesColor="#dedede"
-          :linesWidth="1"
-          :lineLinked="false"
-          :lineOpacity="0.4"
-          :linesDistance="150"
-          :moveSpeed=".30"
-          :hoverEffect="false"
-          hoverMode="grab"
-          :clickEffect="false"
-          clickMode="push"  
-      ></vue-particles>
-    
-  </template>
-  
-  <script>
-  
-  
-  
-  
-  export default {
-    name: 'App',
-    components: {
-  
-     
-  }
-  }
-  </script>
-  
-  <style>
-    * {
-      margin: 0;
-      padding: 0;
-    }
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-  .background {
-    background: rgb(9,4,16);
-  background: linear-gradient(42deg, rgba(9,4,16,1) 0%, rgba(44,21,69,1) 40%, rgba(174,127,217,1) 100%);
-    width: 100%;
-    height: 100%;
-    position: absolute;
-  }
-  
-    
-  </style>
-  
+</template>
+
+<script>
+export default {
+    name: "contactComponent"
+}
+</script>
